470591c3bf9cf99b1f728426847420a7151556d6
Simon Van Den Hende <simonvdhende@outlook.com>
2020-09-24 16:54:00 +0200

    formatting + moving css props here

--- script/app.js
+++ script/app.js
@@ -1,23 +1,39 @@
-"use strict;"
+"use strict;";
 
 let htmlLogo, htmlCard;
 let currentScale = 0;
 
-const listenToLogo = function() {
-    htmlLogo.addEventListener('click', function() {
-        // const newScale = Math.abs(1 - currentScale);
-        const newScale = currentScale ? 0 : 1;
-        // const newScale = + !currentScale;
-
-        htmlCard.style.setProperty('transform', `scale(${newScale})`);
-        currentScale = newScale;
-    })
+function sleep(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-const init = function() {
-    htmlLogo = document.querySelector('.js-logo');
-    htmlCard = document.querySelector('.js-card');
-    listenToLogo();
-}
+const setCardProperties = function () {
+  htmlCard.style.setProperty("transform", "scale(0)");
 
-document.addEventListener('DOMContentLoaded', init);
+  // Only apply transition effect after the scaling.
+  // This means the scaling is without transition, so the user doesnt see it fading away
+  sleep(200).then(() => {
+    htmlCard.style.setProperty("transition", "var(--global-animation-duration)");
+  });
+};
+
+const listenToLogo = function () {
+  htmlLogo.addEventListener("click", function () {
+    // const newScale = Math.abs(1 - currentScale);
+    const newScale = currentScale ? 0 : 1;
+    // const newScale = + !currentScale;
+
+    htmlCard.style.setProperty("transform", `scale(${newScale})`);
+    currentScale = newScale;
+  });
+};
+
+const init = function () {
+  htmlLogo = document.querySelector(".js-logo");
+  htmlCard = document.querySelector(".js-card");
+
+  setCardProperties();
+  listenToLogo();
+};
+
+document.addEventListener("DOMContentLoaded", init);
