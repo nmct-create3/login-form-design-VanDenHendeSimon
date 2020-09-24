6f596a2119056bb9b942aaaa2fb14b0193bb7a1c
Simon Van Den Hende <simonvdhende@outlook.com>
2020-09-24 16:37:51 +0200

    some js for fun

---
+++ script/app.js
@@ -0,0 +1,22 @@
+"use strict;"
+
+let htmlLogo, htmlCard;
+let currentScale = 0;
+
+const listenToLogo = function() {
+    htmlLogo.addEventListener('click', function() {
+        // const newScale = Math.abs(1 - currentScale);
+        const newScale = currentScale ? 0 : 1;
+
+        htmlCard.style.setProperty('transform', `scale(${newScale})`);
+        currentScale = newScale;
+    })
+}
+
+const init = function() {
+    htmlLogo = document.querySelector('.js-logo');
+    htmlCard = document.querySelector('.js-card');
+    listenToLogo();
+}
+
+document.addEventListener('DOMContentLoaded', init);
